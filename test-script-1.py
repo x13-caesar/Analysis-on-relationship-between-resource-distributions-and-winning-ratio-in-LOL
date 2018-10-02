372c4a4dd2e7681cfc367a22b3b8a7e9a62b5972
Caesar <xuqiangwen@outlook.com>
2018-10-02 04:03:22 -0400

    Update test-script-1.py
    1. 简化了raw data输入；
    2. 删掉了不需要的求平均数和求总方差部分（之前写了测试的……）；
    3. 上一版本只拿goldearned来测试，这版把所有列都拿来算方差了；
    4. 用reset_index() 简化了保存agg结果的动作；
    5. 解决了drop命令无效的问题；
    6. 其他一些不用细说的小优化；

--- test-script-1.py
+++ test-script-1.py
@@ -1,70 +1,60 @@
-import csv
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  1 21:22:41 2018
+
+@author: qiangwenxu
+"""
 import pandas as pd
-import numpy as np
+from sklearn.linear_model import LogisticRegression
 
+## 设一个分隔线的function，方便阅读结果
+def next():
+    print("")
+    print("----------")
+    print("")
 
-match=[]
-win=[]
-gold=[]
+## 导入 raw data
+df = pd.DataFrame(pd.read_csv('stats_10000.csv',header=0))
 
-with open('stats_10000.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            #print(f'Column names are {", ".join(row)}' + "\n")
-            line_count += 1
-        else:
-            line_count += 1
-            match.append(row[1])
-            win.append(row[2])
-            gold.append(row[42])
-            
-    print(f'Processed {line_count} lines.' + "\n")
+## 看一下 raw data 的信息
+df.info()
+next()
 
-##calculate average open price
-sum=0
-count=0
-for g in gold:
-    sum += int(g) 
-    count+=1
-mean = sum/count
-print(str(count) + " data. " + str(sum) + " in sum and average gold earned is: " + str(mean) + "\n")
+## 按队伍分组，计算队伍方差 calculate the variance of each team.
+df_team = df.groupby(['match','win']) #categorize df
+df_var = df_team.agg('var').reset_index()
 
-##calculate variance
-sum_sq = 0
-for g in gold:
-    sum_sq += (int(g)-mean)**2
-var = sum_sq/(count-1)
-print("the gold earned variance is: " + str(var) + "\n")
+## 去掉不需要的列（不作为x变量输入）
+df_var = df_var.drop(
+        ['match',
+         'id',
+         'item1',
+         'item2',
+         'item3',
+         'item4',
+         'item5',
+         'item6',
+         'trinket',
+         'largestkillingspree',
+         'largestmultikill',
+         'killingsprees',
+         'doublekills',
+         'triplekills',
+         'quadrakills',
+         'pentakills',
+         'legendarykills',
+         'totdmgdealt', 
+         'largestcrit', 
+         'totdmgtochamp', 
+         'magicdmgtochamp', 
+         'physdmgtochamp',
+         'truedmgtochamp', 
+         'totunitshealed',
+         'firstblood'],
+         axis=1)
 
-print("length of match list " + str(len(match)) + "\n")
+print("### Confirm the data structure ### \n \n",df_var.tail(6))
+next()
+df_var.to_csv('rank_var_test.csv')  #保存一下结果表格
 
-#print 5 rows to confirm the data set is correct.
-for i in range(5):
-    print(match[i])
-    print(win[i])
-    print(gold[i])
-    print("----")
-    
-print("")
-
-##calculate the gold variance of each team.
-df = pd.DataFrame({"match":match,"win":win,"gold":gold},columns =['match','win','gold'], dtype=int)
-print(df.info())
-print("")
-print(df.tail()) #confirm the 'df' dataframe is correct.
-print("")
-
-df_tgold = df.groupby(['match','win']) #categorize df
-df_var_tgold = df_tgold['gold'].agg('var')
-
-print(df_var_tgold.tail(10)) #confrim the df_var_tgold.
-
-df_var_tgold.to_csv('rank_var_test.csv')
-#output the data into a new csv
-
-
-
-
-    
