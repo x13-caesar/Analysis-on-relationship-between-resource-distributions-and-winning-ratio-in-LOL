b464f6d05424be00d6cdb06c24d8e33867b6adb7
Caesar <xuqiangwen@outlook.com>
2018-11-08 00:38:33 -0500

    Update project test.py

--- project test.py
+++ project test.py
@@ -19,60 +19,68 @@ df = pd.DataFrame(pd.read_csv('stats_10000.csv',header=0))
 
 ## 去掉不需要的列（不作为x变量输入）
 ## 可取变量输入数量为10个
-df = df.drop(
-        ['item1',
-         'item2',
-         'item3',
-         'item4',
-         'item5',
-         'item6',
-         'trinket',
-         'largestkillingspree',
-         'largestmultikill',
-         'killingsprees',
-         'longesttimespentliving',
-         'doublekills',
-         'triplekills',
-         'quadrakills',
-         'pentakills',
-         'legendarykills',
-         'magicdmgdealt',
-         'physicaldmgdealt',
-         'truedmgdealt',
-         'largestcrit',
-         'magicdmgtochamp', 
-         'physdmgtaken',
-         'truedmgtaken',
-         'goldspent',
-         'turretkills',
-         'inhibkills',
-         'neutralminionskilled',
-         'ownjunglekills',
-         'enemyjunglekills',
-         'champlvl',
-         'pinksbought',
-         'wardsbought',
-         'wardskilled',
-         'firstblood'],
-         axis=1)
-
-
-##combine KDA
-df['deaths_p'] = df['deaths']
-for i in range(df.shape[0]):
-    if df.at[i,'deaths'] ==0:
-        df.set_value(i, 'deaths_p', 1)
-
-df['KDA'] = (df['kills']+df['assists'])/df['deaths_p']
-
 ## 看一下 raw data 的信息
-df.info()
-next()
+def DataModify(df):
+    df['deaths_p']=df['deaths']
 
-## 按队伍分组，计算队伍方差 calculate the variance of each team.
-df_team = df.groupby(['match','win']) #categorize df
-df_var = df_team.agg('var').reset_index()
+    for i in range(df.shape[0]):
+        if df.at[i, 'deaths'] == 0:
+            df.set_value(i, 'deaths_p', 1)
 
+    df['KDA'] = (df['kills']+df['assists'])/df['deaths_p']
+
+    df = df.drop(
+            ['item1',
+             'item2',
+             'item3',
+             'item4',
+             'item5',
+             'item6',
+             'trinket',
+             'kills',
+             'assists',
+             'deaths',
+             'deaths_p',
+             'largestkillingspree',
+             'largestmultikill',
+             'killingsprees',
+             'longesttimespentliving',
+             'doublekills',
+             'triplekills',
+             'quadrakills',
+             'pentakills',
+             'legendarykills',
+             'magicdmgdealt',
+             'physicaldmgdealt',
+             'truedmgdealt',
+             'largestcrit',
+             'magicdmgtochamp', 
+             'physdmgtaken',
+             'truedmgtaken',
+             'goldspent',
+             'turretkills',
+             'inhibkills',
+             'neutralminionskilled',
+             'ownjunglekills',
+             'enemyjunglekills',
+             'champlvl',
+             'pinksbought',
+             'wardsbought',
+             'wardskilled',
+             'firstblood'], axis=1)
+    
+    ## 按队伍分组，计算队伍方差 calculate the variance of each team.
+    df_team = df.groupby(['match','win']) #categorize df
+    df_var = df_team.agg('var').reset_index()
+    
+    df_var = df_var.drop(['match',
+                          'id'], axis=1)
+    
+    print("### Confirm the data structure ### \n \n",df_var.tail(6))
+          
+    return(df_var)
+          
+df_var = DataModify(df)
 
 print("### Confirm the data structure ### \n \n",df_var.tail(6))
 next()
